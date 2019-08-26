4c5c02d6b0d49e4cebc98c41e7b8dd4228fe0df1
Charles Luxinger <charlesluxinger@hotmail.com>
2019-08-25 22:25:14 -0300

    5. Cadastrando nova categoria com POST e buscando por Id

--- money-api/src/main/java/com/charlesluxinger/money/api/resource/CategoriaResource.java
+++ money-api/src/main/java/com/charlesluxinger/money/api/resource/CategoriaResource.java
@@ -1,11 +1,19 @@
 package com.charlesluxinger.money.api.resource;
 
+import java.net.URI;
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.charlesluxinger.money.api.model.Categoria;
 import com.charlesluxinger.money.api.repository.CategoriaRepository;
@@ -13,12 +21,28 @@ import com.charlesluxinger.money.api.repository.CategoriaRepository;
 @RestController
 @RequestMapping("/categorias")
 public class CategoriaResource {
-	
+
 	@Autowired
 	private CategoriaRepository categoriaRepository;
-	
+
 	@GetMapping
-	public List<Categoria> findAll(){
+	public List<Categoria> findAll() {
 		return categoriaRepository.findAll();
+	}
+
+	@PostMapping
+	public ResponseEntity<Categoria> insert(@RequestBody Categoria categoria, HttpServletResponse response) {
+		Categoria categoriaInserted = categoriaRepository.save(categoria);
+
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}")
+				.buildAndExpand(categoriaInserted.getId()).toUri();
+		
+		return ResponseEntity.created(uri).body(categoriaInserted);
+		
+	}
+	
+	@GetMapping("/{id}")
+	public Categoria findById(@PathVariable Long id) {
+		  return this.categoriaRepository.findById(id).orElse(null);
 	}
 }
