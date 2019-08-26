c2b1124e0cf80293e65a5465374f6a796a6a07e9
Charles Luxinger <charlesluxinger@hotmail.com>
2019-08-25 21:49:10 -0300

    4. Consultando primeiro recurso com GET

---
+++ money-api/src/main/java/com/charlesluxinger/money/api/resource/CategoriaResource.java
@@ -0,0 +1,24 @@
+package com.charlesluxinger.money.api.resource;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.charlesluxinger.money.api.model.Categoria;
+import com.charlesluxinger.money.api.repository.CategoriaRepository;
+
+@RestController
+@RequestMapping("/categorias")
+public class CategoriaResource {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
+	@GetMapping
+	public List<Categoria> findAll(){
+		return categoriaRepository.findAll();
+	}
+}
