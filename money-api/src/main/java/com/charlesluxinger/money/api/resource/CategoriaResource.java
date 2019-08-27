ae740b5421d1254c6c9d217c59af46b2ab818356
Charles Luxinger <charlesluxinger@hotmail.com>
2019-08-26 23:57:57 -0300

    11. Usando eventos para adicionar header Location

--- money-api/src/main/java/com/charlesluxinger/money/api/resource/CategoriaResource.java
+++ money-api/src/main/java/com/charlesluxinger/money/api/resource/CategoriaResource.java
@@ -1,6 +1,5 @@
 package com.charlesluxinger.money.api.resource;
 
-import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -8,6 +7,8 @@ import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,8 +16,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.charlesluxinger.money.api.event.InsertResourceEvent;
 import com.charlesluxinger.money.api.model.Categoria;
 import com.charlesluxinger.money.api.repository.CategoriaRepository;
 
@@ -27,6 +28,9 @@ public class CategoriaResource {
 	@Autowired
 	private CategoriaRepository categoriaRepository;
 
+	@Autowired
+	private ApplicationEventPublisher publisher;
+
 	@GetMapping
 	public List<Categoria> findAll() {
 		return categoriaRepository.findAll();
@@ -36,11 +40,9 @@ public class CategoriaResource {
 	public ResponseEntity<Categoria> insert(@Valid @RequestBody Categoria categoria, HttpServletResponse response) {
 		Categoria categoriaInserted = categoriaRepository.save(categoria);
 
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}")
-				.buildAndExpand(categoriaInserted.getId()).toUri();
+		publisher.publishEvent(new InsertResourceEvent(this, response, categoriaInserted.getId()));
 
-		return ResponseEntity.created(uri).body(categoriaInserted);
-
+		return ResponseEntity.status(HttpStatus.CREATED).body(categoriaInserted);
 	}
 
 	@GetMapping("/{id}")
--- money-api/src/main/java/com/charlesluxinger/money/api/resource/PessoaResource.java
+++ money-api/src/main/java/com/charlesluxinger/money/api/resource/PessoaResource.java
@@ -1,6 +1,5 @@
 package com.charlesluxinger.money.api.resource;
 
-import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -8,6 +7,8 @@ import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,8 +16,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.charlesluxinger.money.api.event.InsertResourceEvent;
 import com.charlesluxinger.money.api.model.Pessoa;
 import com.charlesluxinger.money.api.repository.PessoaRepository;
 
@@ -27,6 +28,9 @@ public class PessoaResource {
 	@Autowired
 	private PessoaRepository pessoaRepository;
 
+	@Autowired
+	private ApplicationEventPublisher publisher;
+
 	@GetMapping
 	public List<Pessoa> findAll() {
 		return pessoaRepository.findAll();
@@ -36,11 +40,9 @@ public class PessoaResource {
 	public ResponseEntity<Pessoa> insert(@Valid @RequestBody Pessoa pessoa, HttpServletResponse response) {
 		Pessoa pessoaInserted = pessoaRepository.save(pessoa);
 
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}")
-				.buildAndExpand(pessoaInserted.getId()).toUri();
+		publisher.publishEvent(new InsertResourceEvent(this, response, pessoaInserted.getId()));
 
-		return ResponseEntity.created(uri).body(pessoaInserted);
-
+		return ResponseEntity.status(HttpStatus.CREATED).body(pessoaInserted);
 	}
 
 	@GetMapping("/{id}")
