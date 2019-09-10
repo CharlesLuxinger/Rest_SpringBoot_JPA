733396168826eaedfa205d8e66b233e7adac7c04
Charles Luxinger <charlesluxinger@hotmail.com>
2019-09-09 21:35:53 -0300

    12. Implementado método Delete

--- money-api/src/main/java/com/charlesluxinger/money/api/exceptionshandler/MoneyExceptionsHandler.java
+++ money-api/src/main/java/com/charlesluxinger/money/api/exceptionshandler/MoneyExceptionsHandler.java
@@ -7,6 +7,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +15,7 @@ import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
@@ -77,5 +79,17 @@ public class MoneyExceptionsHandler extends ResponseEntityExceptionHandler {
 			return messageDeveloper;
 		}
 
+	}
+
+	@ExceptionHandler({ EmptyResultDataAccessException.class })
+	public ResponseEntity<Object> handleEmptyResultDataAccessException(WebRequest request,
+			EmptyResultDataAccessException ex) {
+
+		String messageUser = messageSource.getMessage("recurso.nao-encontrado", null, LocaleContextHolder.getLocale());
+		String messageDeveloper = ex.toString();
+
+		List<Erro> erros = Arrays.asList(new Erro(messageUser, messageDeveloper));
+
+		return handleExceptionInternal(ex, erros, new HttpHeaders(), HttpStatus.NOT_FOUND, request);
 	}
 }
--- money-api/src/main/java/com/charlesluxinger/money/api/resource/PessoaResource.java
+++ money-api/src/main/java/com/charlesluxinger/money/api/resource/PessoaResource.java
@@ -10,11 +10,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.charlesluxinger.money.api.event.InsertResourceEvent;
@@ -51,4 +53,13 @@ public class PessoaResource {
 
 		return (pessoa.isPresent() ? ResponseEntity.ok(pessoa.get()) : ResponseEntity.notFound().build());
 	}
+
+	@DeleteMapping("/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void remove(@PathVariable long id) {
+		pessoaRepository.deleteById(id);
+	}
+
+
+
 }
