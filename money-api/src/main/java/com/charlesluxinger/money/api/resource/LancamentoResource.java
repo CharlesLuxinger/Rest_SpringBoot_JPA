a0e0b236ef6036e192bf28e579db1dbbb28f7eda
Charles Luxinger <charlesluxinger@hotmail.com>
2019-09-14 17:49:34 -0300

    22. Removendo lançamentos

--- money-api/src/main/java/com/charlesluxinger/money/api/resource/LancamentoResource.java
+++ money-api/src/main/java/com/charlesluxinger/money/api/resource/LancamentoResource.java
@@ -13,12 +13,14 @@ import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.charlesluxinger.money.api.event.InsertResourceEvent;
@@ -69,6 +71,12 @@ public class LancamentoResource {
 		Optional<Lancamento> lancamento = this.lancamentoRepository.findById(id);
 
 		return (lancamento.isPresent() ? ResponseEntity.ok(lancamento.get()) : ResponseEntity.notFound().build());
+	}
+
+	@DeleteMapping("/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void remove(@PathVariable long id) {
+		lancamentoRepository.deleteById(id);
 	}
 
 	@ExceptionHandler({PessoaInexistenteOuInativaException.class})
