d313358b563b042dc0654949b1e6bd1f5d116ee2
Charles Luxinger <charlesluxinger@hotmail.com>
2019-09-20 00:34:34 -0300

    32. Movendo o usuário para o banco de dados com senha Encodada

--- money-api/src/main/java/com/charlesluxinger/money/api/config/AuthorizationServerConfig.java
+++ money-api/src/main/java/com/charlesluxinger/money/api/config/AuthorizationServerConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
@@ -12,18 +13,23 @@ import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
+import com.charlesluxinger.money.api.security.utils.PasswordGenerator;
+
 @Configuration
 @EnableAuthorizationServer
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
 
 	@Autowired
 	private AuthenticationManager authenticationManager;
+	
+	@Autowired
+	private UserDetailsService userDetailsService;
 
 	@Override
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
 		clients.inMemory()
 				.withClient("angular")
-				.secret("@ngul@r")
+				.secret(PasswordGenerator.getNewEncodedPassword("@ngul@r"))
 				.scopes("read", "write")
 				.authorizedGrantTypes("password", "refresh_token")
 				.accessTokenValiditySeconds(20)
@@ -33,9 +39,10 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	@Override
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
 		endpoints.tokenStore(tokenStore())
-				.accessTokenConverter(accessTokenConverter())
+				.accessTokenConverter(this.accessTokenConverter())
 				.reuseRefreshTokens(false)
-				.authenticationManager(authenticationManager);
+				.authenticationManager(this.authenticationManager)
+				.userDetailsService(this.userDetailsService);
 	}
 
 	@Bean
