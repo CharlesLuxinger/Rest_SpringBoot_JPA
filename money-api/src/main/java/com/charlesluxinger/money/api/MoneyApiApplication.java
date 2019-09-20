f77a9e7245ed9514c4ed71941749f7121ef14057
Charles Luxinger <charlesluxinger@hotmail.com>
2019-09-19 22:50:21 -0300

    30. CORS
    Cross-origin resource sharing Compartilhamento de recursos de origem
    cruzada
    
    Global e por recurso.

--- money-api/src/main/java/com/charlesluxinger/money/api/MoneyApiApplication.java
+++ money-api/src/main/java/com/charlesluxinger/money/api/MoneyApiApplication.java
@@ -6,6 +6,9 @@ import javax.annotation.PostConstruct;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class MoneyApiApplication {
@@ -17,6 +20,16 @@ public class MoneyApiApplication {
 	@PostConstruct
 	void started() {
 		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/*").allowedOrigins("http://localhost:8000");
+			}
+		};
 	}
 
 }
